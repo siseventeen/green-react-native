f4d1c77aca614dff4dd98dc124da8e40714e758b
siseventeen <176776401@qq.com>
2019-11-17 02:33:06 -0600

    dialog test

---
+++ match-my-songs/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PlaylistPage from './src/playlist.page';
+
+
+export default function App() {  
+  return (
+      <PlaylistPage />
+  );    
+}
+
+
+
